ee98c854eff0b65b61aea8a0de992699c8d74ded
OlAntZin18 <90936695+OlAntZin18@users.noreply.github.com>
2022-01-11 11:59:00 +0300

    Update task15.jl

--- task15.jl
+++ task15.jl
@@ -1,7 +1,7 @@
 function lab_15(r::Robot)
     path = go_to_west_south_corner_and_return_path!(r; go_around_barriers = true)
 
-    for i ∈ (North, East, South, West)
+    for i ∈ (Nord, Ost, Sud, West)
         go_to_border_and_return_path!(r, i; markers = true)
     end
     go_by_path!(r, path)
