00821634b893b4b5e10ce29545962133015fa42c
OlAntZin18 <90936695+OlAntZin18@users.noreply.github.com>
2022-01-11 11:55:41 +0300

    Update task16.jl

--- task16.jl
+++ task16.jl
@@ -1,11 +1,11 @@
 function lab_16(r::Robot)
     path = go_to_west_south_corner_and_return_path!(r; go_around_barriers = true)
 
-    side = East
+    side = Ost
     while !isborder(r,North)
         go_to_border_and_return_path!(r, side; markers = true, go_around_barriers = true)
         side = inverse_side(side)
-        go!(r, North)
+        go!(r, Nord)
     end
 
     go_to_border_and_return_path!(r, side; markers = true, go_around_barriers = true)
