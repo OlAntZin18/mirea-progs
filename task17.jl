e6628eaaa377604dec63ec8d8bb9968f04677152
OlAntZin18 <90936695+OlAntZin18@users.noreply.github.com>
2022-01-11 11:55:13 +0300

    Update task17.jl

--- task17.jl
+++ task17.jl
@@ -4,12 +4,12 @@ function lab_17(r::Robot)
     x = go_to_border_come_back_and_return_distance!(r, East; go_around_barriers = true)
     marks_to_do = x
     while !isborder(r,Nord) && marks_to_do > 0
-        go!(r, East; steps = marks_to_do, go_around_barriers = true, markers = true)
+        go!(r, Ost; steps = marks_to_do, go_around_barriers = true, markers = true)
         go!(r, West; steps = marks_to_do, go_around_barriers = true, markers = false)
         move!(r,Nord)
         marks_to_do -= 2
     end
-    go!(r, East; steps = marks_to_do, go_around_barriers = true, markers = true)
+    go!(r, Ost; steps = marks_to_do, go_around_barriers = true, markers = true)
     go!(r, West; steps = marks_to_do, go_around_barriers = true, markers = false)
 
     go_to_west_south_corner_and_return_path!(r; go_around_barriers = true)
